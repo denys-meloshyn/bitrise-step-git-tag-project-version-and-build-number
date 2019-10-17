3e618b8e4041c824d157e4ea1d22f471be330aa4
Denys Meloshyn <denys.meloshyn@gmail.com>
2019-10-17 22:50:19 +0200

    v1.0.4 (#8)
    * Add some flexibility to account for more use cases: (#1)
    
    * Add some flexibility to account for more use cases:
    
     - add lightweight-tag toggle as config option
     - rework tag format to make use of placeholders for version and build number
     - fix bug: now always tag the current codebase instead of HEAD
    
    * fix typo: take build number string instead of version string as replacement for _BUILD_
    
    * Keep previous formatting
    
    * format script
    
    * Extract build version from MARKETING_VERSION
    
    * Add validation
    
    * Update description

--- step.sh
+++ step.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # set -ex
 
-read_dom () {
-    local IFS=\>
-    read -d \< ENTITY CONTENT
+read_dom() {
+  local IFS=\>
+  read -d \< ENTITY CONTENT
 }
 
 CFBundleVersion=""
@@ -13,48 +13,72 @@ CFBundleShortVersionString=""
 CFBundleShortVersionStringKey=false
 
 if [ -z "$bitrise_tag_info_plist_path" ]; then
-    echo "bitrise_tag_info_plist_path is empty"
-    exit 1
+  echo "bitrise_tag_info_plist_path is empty"
+  exit 1
 fi
 
 while read_dom; do
-  if [[ $CFBundleShortVersionStringKey = true ]]; then
-    if [ $ENTITY = "string" ] ; then
+  if [[ $CFBundleShortVersionStringKey == true ]]; then
+    if [ $ENTITY = "string" ]; then
       CFBundleShortVersionString=$CONTENT
       CFBundleShortVersionStringKey=false
     fi
   fi
 
-  if [[ $CFBundleVersionKey = true ]]; then
-    if [ $ENTITY = "string" ] ; then
+  if [[ $CFBundleVersionKey == true ]]; then
+    if [ $ENTITY = "string" ]; then
       CFBundleVersion=$CONTENT
       CFBundleVersionKey=false
     fi
   fi
 
-  if [[ $CONTENT = "CFBundleShortVersionString" ]] ; then
+  if [[ $CONTENT == "CFBundleShortVersionString" ]]; then
     CFBundleShortVersionStringKey=true
   fi
 
-  if [[ $CONTENT = "CFBundleVersion" ]] ; then
+  if [[ $CONTENT == "CFBundleVersion" ]]; then
     CFBundleVersionKey=true
   fi
-done < $bitrise_tag_info_plist_path
+done <$bitrise_tag_info_plist_path
 
 if [ -z "$CFBundleShortVersionString" ]; then
-    echo "CFBundleShortVersionString is empty"
-    exit 1
+  echo "CFBundleShortVersionString is empty"
+  exit 1
 fi
 
 if [ -z "$CFBundleVersion" ]; then
-    echo "CFBundleVersion is empty"
+  echo "CFBundleVersion is empty"
+  exit 1
+fi
+
+if [[ $CFBundleShortVersionString == *MARKETING_VERSION* ]]; then
+  echo "Exctract version number from xcodeproj"
+
+  if [ -z "$bitrise_tag_xcodeproj_path" ]; then
+    echo "bitrise_tag_xcodeproj_path is empty"
     exit 1
+  fi
+
+  MARKETING_VERSION=""
+  LINES=$(sed -n '/MARKETING_VERSION/=' "$bitrise_tag_xcodeproj_path/project.pbxproj")
+  for LINE in $LINES; do
+    MARKETING_VERSION=$(sed -n "$LINE"p "$bitrise_tag_xcodeproj_path"/project.pbxproj)
+    MARKETING_VERSION="${MARKETING_VERSION#*= }"
+    MARKETING_VERSION="${MARKETING_VERSION%;}"
+  done
+
+  if [ -z "$MARKETING_VERSION" ]; then
+    echo "MARKETING_VERSION is empty"
+    exit 1
+  fi
+
+  CFBundleShortVersionString=$MARKETING_VERSION
 fi
 
 TAG_NAME=""
 
 if [ -z "$bitrise_tag_format" ]; then
-    printf -v TAG_NAME "v%s(%s)" "$CFBundleShortVersionString" "$CFBundleVersion"
+  printf -v TAG_NAME "v%s(%s)" "$CFBundleShortVersionString" "$CFBundleVersion"
 else
   if [[ $bitrise_tag_format == *"_VERSION_"* ]]; then
     name="${bitrise_tag_format//_VERSION_/$CFBundleShortVersionString}"
@@ -64,17 +88,16 @@ else
     # Support previous integration
     printf -v TAG_NAME "v%s(%s)" "$CFBundleShortVersionString" "$CFBundleVersion"
   fi
-
 fi
-echo $TAG_NAME
+echo "New tag: $TAG_NAME"
 
 git checkout "$BITRISE_GIT_BRANCH"
 git tag "$TAG_NAME" "$GIT_CLONE_COMMIT_HASH"
 
-if [[ $use_lightweight_tag = "yes" ]]; then
-    git push origin "$TAG_NAME"
+if [[ $use_lightweight_tag == "yes" ]]; then
+  git push origin "$TAG_NAME"
 else
-    git push --tags
+  git push --tags
 fi
 
 exit 0
