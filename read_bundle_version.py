dc68aaa06355c40857620c1b5ab75dd16c3f2299
Denys Meloshyn <denys.meloshyn@gmail.com>
2021-06-16 23:18:40 +0200

    Update unit tests

--- read_bundle_version.py
+++ read_bundle_version.py
@@ -57,34 +57,22 @@ def read_from_plist_or_xcode(plist_key, xcode_key, info_plist_path, xcode_path, 
 
     return value
 
-# read_from_plist_or_xcode(
-#     plist_key='CFBundleVersion',
-#     xcode_key='MARKETING_VERSION',
-#     info_plist_path='/Users/denys/Projects/bitrise-step-git-tag-project-version-and-build-number/_tmp'
-#                     '/test_info_plist/Test/Info.plist',
-#     xcode_path=None,
-#     xcworkspace_path=None,
-#     scheme=None,
-#     config=None)
-#
-# read_from_plist_or_xcode(
-#     plist_key='CFBundleVersion',
-#     xcode_key='MARKETING_VERSION',
-#     info_plist_path='/Users/denys/Projects/bitrise-step-git-tag-project-version-and-build-number/_tmp'
-#                     '/test_build_settings/Test/Info.plist',
-#     xcode_path='/Users/denys/Projects/bitrise-step-git-tag-project-version-and-build-number/_tmp'
-#                '/test_build_settings/Test.xcodeproj',
-#     xcworkspace_path=None,
-#     scheme=None,
-#     config=None)
-#
-# read_from_plist_or_xcode(
-#     plist_key='CFBundleVersion',
-#     xcode_key='MARKETING_VERSION',
-#     info_plist_path='/Users/denys/Projects/bitrise-step-git-tag-project-version-and-build-number/_tmp'
-#                     '/test_workspace_config/Test/Info.plist',
-#     xcode_path=None,
-#     xcworkspace_path='/Users/denys/Projects/bitrise-step-git-tag-project-version-and-build-number/_tmp'
-#                      '/test_workspace_config/Test.xcworkspace',
-#     scheme='Test',
-#     config='Config')
+
+def read_bundle_version(info_plist_path, xcode_path, xcworkspace_path, scheme, config):
+    return read_from_plist_or_xcode(plist_key='CFBundleVersion',
+                                    xcode_key='CURRENT_PROJECT_VERSION',
+                                    info_plist_path=info_plist_path,
+                                    xcode_path=xcode_path,
+                                    xcworkspace_path=xcworkspace_path,
+                                    scheme=scheme,
+                                    config=config)
+
+
+def read_short_bundle_version(info_plist_path, xcode_path, xcworkspace_path, scheme, config):
+    return read_from_plist_or_xcode(plist_key='CFBundleShortVersionString',
+                                    xcode_key='MARKETING_VERSION',
+                                    info_plist_path=info_plist_path,
+                                    xcode_path=xcode_path,
+                                    xcworkspace_path=xcworkspace_path,
+                                    scheme=scheme,
+                                    config=config)
--- test_read_from_plist_or_xcode.py
+++ test_read_from_plist_or_xcode.py
@@ -1,24 +1,20 @@
 from unittest import TestCase
 
-from read_bundle_version import read_from_plist_or_xcode
+from read_bundle_version import read_short_bundle_version
 
 
 class Test(TestCase):
     def test_read_from_plist(self):
-        result = read_from_plist_or_xcode(
-            plist_key='CFBundleVersion',
-            xcode_key=None,
+        result = read_short_bundle_version(
             info_plist_path='./_tmp/test_info_plist/Test/Info.plist',
             xcode_path=None,
             xcworkspace_path=None,
             scheme=None,
             config=None)
-        self.assertEqual(result, "1")
+        self.assertEqual(result, "1.0")
 
     def test_read_from_xcode_build_settings(self):
-        result = read_from_plist_or_xcode(
-            plist_key='CFBundleVersion',
-            xcode_key='MARKETING_VERSION',
+        result = read_short_bundle_version(
             info_plist_path='./_tmp/test_build_settings/Test/Info.plist',
             xcode_path='./_tmp/test_build_settings/Test.xcodeproj',
             xcworkspace_path=None,
@@ -27,23 +23,19 @@ class Test(TestCase):
         self.assertEqual(result, "1.0")
 
     def test_read_from_xcode_config(self):
-        result = read_from_plist_or_xcode(
-            plist_key='CFBundleVersion',
-            xcode_key='MARKETING_VERSION',
+        result = read_short_bundle_version(
             info_plist_path='./_tmp/test_config/Test/Info.plist',
             xcode_path='./_tmp/test_config/Test.xcodeproj',
             xcworkspace_path=None,
             scheme=None,
             config=None)
-        self.assertEqual(result, "1")
+        self.assertEqual(result, "1.0")
 
     def test_read_from_workspace_config(self):
-        result = read_from_plist_or_xcode(
-            plist_key='CFBundleVersion',
-            xcode_key='MARKETING_VERSION',
+        result = read_short_bundle_version(
             info_plist_path='./_tmp/test_workspace_config/Test/Info.plist',
-            xcode_path='./_tmp/test_workspace_config/Test.xcodeproj',
-            xcworkspace_path=None,
-            scheme=None,
-            config=None)
-        self.assertEqual(result, "1")
+            xcode_path=None,
+            xcworkspace_path='./_tmp/test_workspace_config/Test.xcworkspace',
+            scheme='Test',
+            config='Config')
+        self.assertEqual(result, "1.0")
