ec195b0487fbdc469dc20c7db7b1b02eb3efbf65
Denys Meloshyn <denys.meloshyn@gmail.com>
2021-06-16 20:27:01 +0200

    Create 3 test projects with different configurations

---
+++ _tmp/test_info_plist/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Denys Meloshyn on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
